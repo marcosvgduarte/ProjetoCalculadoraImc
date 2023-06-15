f63ea3583322f19c4847fb0622b9de5799b8ea87
marcosvgduarte <136664198+marcosvgduarte@users.noreply.github.com>
2023-06-15 00:35:25 -0300

    Add files via upload

---
+++ projeto-calculadora-imc/README.md
@@ -0,0 +1,32 @@
+## Sobre
+Descrição projeto proposto: Calculadora de IMC (Índice de Massa Corporal).
+
+HTML, CSS, JS.
+
+Vamos construir um projeto simples, capaz de calcular o IMC (índice de massa corporal), de acordo com a seguinte especificação:
+
+IMC = Peso / Altura²
+
+O resultado deve ser expresso, conforme os valores abaixo:
+- Magreza, quando o resultado é menor que 18,5 kg/m2;
+- Normal, quando o resultado está entre 18,5 e 24,9 kg/m2;
+- Sobrepeso, quando o resultado está entre 24,9 e 30 kg/m2;
+- Obesidade, quando o resultado é maior que 30 kg/m2;
+
+Pode ser implementado na tecnologia que melhor convier, com as bibliotecas que julgar necessário... entrega deve ser feita através de um link para o repositório no github
+
+Para melhor visualização, o projeto encontra-se em deploy [AQUI](https://github.com/marcosvgduarte/ProjetoCalculadoraImc).
+
+## Tecnologias utilizadas
+Foram utilizadas as seguintes tecnologias:
+- HTML;
+- CSS;
+- Vanilla JavaScript.
+
+## Melhorias futuras
+1. Ajustar a responsividade.
+2. Ajustar as máscaras dos campos.
+3. Aplicar jQuery.
+
+## Desenvolvido por:
+- Marcos Duarte.
---
+++ projeto-calculadora-imc/assets/js/script.js
@@ -0,0 +1,94 @@
+// declarando as variáveis resultado, altura e peso e utilizando o dom.
+var imcResultado = document.querySelector("#imc-resultado");
+var classificadorResultado = document.querySelector("#classificador-resultado");
+var altura = document.querySelector("#input-altura");
+var peso = document.querySelector("#input-peso");
+
+
+window.onload = function () {
+  restartGif();
+
+  formatarCampoAltura();
+
+  formatarCampoPeso();
+}
+
+function mostrarResultado() {
+  const imc = calcularImc();
+
+  if (altura.value === '' || peso.value === '') {
+    imcResultado.innerHTML = 'Por favor, digite um valor válido.';
+  } else if (imc < 18.5) {
+    imcResultado.innerHTML = `Seu IMC é: ${imc.toFixed(2)}.`;
+    classificadorResultado.innerHTML = 'Você está com MAGREZA.';
+  } else if (imc > 18.5 && 24.9 >= imc) {
+    imcResultado.innerHTML = `Seu IMC é: ${imc.toFixed(2)}.`;
+    classificadorResultado.innerHTML = 'Você está com o peso NORMAL.';
+  } else if (25 < imc && 29.9 >= imc) {
+    imcResultado.innerHTML = `Seu IMC é: ${imc.toFixed(2)}.`;
+    classificadorResultado.innerHTML = 'Você está com SOBREPESO.';
+  } else {
+    imcResultado.innerHTML = `Seu IMC é: ${imc.toFixed(2)}.`;
+    classificadorResultado.innerHTML = 'Você está com OBESIDADE.';
+  }
+}
+
+function calcularImc() {
+  return peso.value / (altura.value * altura.value);
+}
+
+function restartGif() {
+  var gif = document.getElementById('gym')
+
+  setInterval(function () {
+    gif.src = gif.src.replace(/\?.*/,
+      function () {
+        return '?' + new Date()
+      })
+  }, 7300)
+}
+
+function formatarCampoAltura() {
+  var altura = document.getElementById("input-altura");
+
+  var flag = false;
+
+  altura.addEventListener("input", function (event) {
+    pattern = /^\d+[.]?\d{0,2}$/;
+    if (pattern.test(this.value)) {
+      flag = true;
+      l = this.value;
+      if (flag && this.value.length > 1) return this.value;
+      else flag = false;
+    } else if (!pattern.test(this.value) && flag == true) {
+      return (this.value = l);
+    } else if (!pattern.test(this.value) && flag == false) {
+      return (this.value = "");
+    }
+  });
+}
+
+function formatarCampoPeso() {
+  var peso = document.getElementById("input-peso");
+
+  var flag = false;
+
+  peso.addEventListener("input", function (event) {
+    pattern = /^\d+[.]?\d{0,2}$/;
+    if (pattern.test(this.value)) {
+      flag = true;
+      l = this.value;
+      if (flag && this.value.length > 1) return this.value;
+      else flag = false;
+    } else if (!pattern.test(this.value) && flag == true) {
+      return (this.value = l);
+    } else if (!pattern.test(this.value) && flag == false) {
+      return (this.value = "");
+    }
+  });
+}
+
+function limparCampos() {
+  altura.value = "";
+  peso.value = "";
+}
